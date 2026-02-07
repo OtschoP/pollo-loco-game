081ee7108ce18166646cfa2a967ae43810f26282
OtschoP <35844438+OtschoP@users.noreply.github.com>
2026-02-07 17:07:23 +0100

    add world.class.js

--- js/game.js
+++ js/game.js
@@ -1,15 +1,11 @@
 let canvas;
 let ctx;
-let character = new Character();
-let enemies = [
-    new Chicken(),
-    new Chicken(),
-    new Chicken
-]
+let world = new World();
+
 
 function init() {
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
 
-    console.log('My Character is', character);
+    console.log('My Character is', world.character);
 }
