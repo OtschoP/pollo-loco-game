c13f33a13c5ce2248a421f12c8d415981ca79855
OtschoP <35844438+OtschoP@users.noreply.github.com>
2026-02-09 17:30:30 +0100

    add character in canvas and optimize y

--- models/moveable-object.class.js
+++ models/moveable-object.class.js
@@ -1,7 +1,9 @@
 class MoveableObject {
-    x = 120;
-    y = 400;
+    x = 100;
+    y = 250;
     img;
+    height = 150;
+    width = 100;
 
     loadImage(path) {
         this.img = new Image(); // this,img = document.getElementById('img') <img id="image" src>
