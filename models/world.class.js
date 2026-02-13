178cf5762b07ba94224fcc5550e01c548c1d6c31
OtschoP <35844438+OtschoP@users.noreply.github.com>
2026-02-13 16:16:40 +0100

    layered background

--- models/world.class.js
+++ models/world.class.js
@@ -27,13 +27,12 @@ class World {
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-
-        this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
-
         this.ctx.drawImage(this.air.img, this.air.x, this.air.y, this.air.width, this.air.height);
-        this.ctx.drawImage(this.layerone.img, this.layerone.x, this.layerone.y, this.layerone.width, this.layerone.height);
         this.ctx.drawImage(this.layertwo.img, this.layertwo.x, this.layertwo.y, this.layertwo.width, this.layertwo.height);
         this.ctx.drawImage(this.layerthree.img, this.layerthree.x, this.layerthree.y, this.layerthree.width, this.layerthree.height);
+        this.ctx.drawImage(this.layerone.img, this.layerone.x, this.layerone.y, this.layerone.width, this.layerone.height);
+        
+        this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
 
 
         this.enemies.forEach(enemy => {
