ceb63054d8dde64ab1d7dd11fdf3525f002987ae
OtschoP <35844438+OtschoP@users.noreply.github.com>
2026-02-14 12:17:00 +0100

    corrected cordinates

--- models/world.class.js
+++ models/world.class.js
@@ -9,7 +9,7 @@ class World {
         new Cloud()
     ];
     backgroundObjects = [
-        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 0, 80)
+        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 0, 0)
     ];
 
 
@@ -26,10 +26,10 @@ class World {
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-        this.addToMap(this.character);
         this.addObjectsToMap(this.backgroundObjects); 
+        this.addToMap(this.character);
         this.addObjectsToMap(this.enemies); 
-        this.addObjectsToMap(this.clouds); 
+        this.addObjectsToMap(this.clouds);  
 
         // draw wird immer wieder aufgerufen
         let self = this;
