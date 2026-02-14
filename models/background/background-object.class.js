9cd695a82f2d93e129a00ef0648c8a50bf570a8a
OtschoP <35844438+OtschoP@users.noreply.github.com>
2026-02-14 12:39:52 +0100

    add more backgrounds

--- models/background/background-object.class.js
+++ models/background/background-object.class.js
@@ -3,10 +3,11 @@ class BackgroundObject extends MoveableObject {
 
     width = 720;
     height = 480;
-    constructor(imagePath, x, y) {
+
+    constructor(imagePath, x) {
         super().loadImage(imagePath);
         this.x = x;
-        this.y = y;
+        this.y = 480 - this.height;
     }
 
 }
