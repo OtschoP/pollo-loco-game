9a088502f134298158f7504ad63fe9f03dd45740
OtschoP <35844438+OtschoP@users.noreply.github.com>
2026-02-16 16:19:54 +0100

    animation for character

--- models/moveable-object/moveable-object.class.js
+++ models/moveable-object/moveable-object.class.js
@@ -4,17 +4,26 @@ class MoveableObject {
     img;
     height = 150;
     width = 100;
+    imageCache = {};
 
     loadImage(path) {
         this.img = new Image(); // this,img = document.getElementById('img') <img id="image" src>
         this.img.src = path;
     }
 
+    loadImages(arr) {
+        arr.forEach((path) => {
+            let img = new Image();
+            img.src = path;
+            this.imageCache[path] = path;
+        });
+    }
+
     moveRight() {
         console.log('Moving right');
     }
 
-    moveLeft(){
-        
+    moveLeft() {
+
     }
 }
