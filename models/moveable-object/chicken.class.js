3f653e05f3cdb4b4e769b3dbc2e05689bdaf3d06
OtschoP <35844438+OtschoP@users.noreply.github.com>
2026-02-16 14:15:42 +0100

    chicken movement

--- models/moveable-object/chicken.class.js
+++ models/moveable-object/chicken.class.js
@@ -2,9 +2,16 @@ class Chicken extends MoveableObject {
     y = 350;
     height = 80;
 
-    constructor(){
+    constructor() {
         super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
 
         this.x = 200 + Math.random() * 500;
+        this.animate();
+    }
+
+    animate() {
+        setInterval(() => {
+            this.x -= 0.5;
+        }, 1000 / 60);
     }
 }
