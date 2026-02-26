6815313d62d623f6bc9615cbb193bcb36ef55e4c
OtschoP <35844438+OtschoP@users.noreply.github.com>
2026-02-26 17:37:12 +0100

    cloud2.class.js kann ich jetzt löschen

--- models/world.class.js
+++ models/world.class.js
@@ -14,7 +14,7 @@ class World {
         this.setWorld();
     }
 
-    setWorld(){
+    setWorld() {
         this.character.world = this;
     }
 
@@ -23,11 +23,11 @@ class World {
 
         this.ctx.translate(this.camera_x, 0);
 
-        this.addObjectsToMap(this.level.backgroundObjects); 
-        this.addObjectsToMap(this.level.clouds);  
+        this.addObjectsToMap(this.level.backgroundObjects);
+        this.addObjectsToMap(this.level.clouds);
         this.addToMap(this.character);
-        this.addObjectsToMap(this.level.enemies); 
-         
+        this.addObjectsToMap(this.level.enemies);
+
 
         this.ctx.translate(-this.camera_x, 0);
 
@@ -38,14 +38,14 @@ class World {
         });
     }
 
-    addObjectsToMap(objects){
+    addObjectsToMap(objects) {
         objects.forEach(o => {
             this.addToMap(o);
         })
     }
 
     addToMap(mo) {
-        if(mo.otherDirection){
+        if (mo.otherDirection) {
             this.ctx.save();
             this.ctx.translate(mo.width, 0);
             this.ctx.scale(-1, 1);
