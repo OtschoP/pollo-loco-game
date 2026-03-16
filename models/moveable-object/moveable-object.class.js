dfc7753b394520a05d50439fb7ffd2505f1dae18
OtschoP <35844438+OtschoP@users.noreply.github.com>
2026-03-16 13:53:39 +0100

    add drawable object and status-bar class

--- models/moveable-object/moveable-object.class.js
+++ models/moveable-object/moveable-object.class.js
@@ -1,11 +1,4 @@
-class MoveableObject {
-    x = 100;
-    y = 280;
-    img;
-    height = 150;
-    width = 100;
-    imageCache = {};
-    currentImage = 0;
+class MoveableObject extends DrawableObject {
     currentAnimation = 0;
     speed = 0.15;
     otherDirection = false;
@@ -31,23 +24,6 @@ class MoveableObject {
 
     isAboveGround() {
         return this.y < 180;
-    }
-
-    loadImage(path) {
-        this.img = new Image(); // this,img = document.getElementById('img') <img id="image" src>
-        this.img.src = path;
-    }
-
-    loadImages(arr) {
-        arr.forEach((path) => {
-            let img = new Image();
-            img.src = path;
-            this.imageCache[path] = img;
-        });
-    }
-
-    draw(ctx) {
-        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
     }
 
     drawFrame(ctx) {
