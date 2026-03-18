c00fafb85f5b41d8ace4cff69fe467d525b77bfe
OtschoP <35844438+OtschoP@users.noreply.github.com>
2026-03-18 06:29:26 +0100

    statusbar placed

--- models/world.class.js
+++ models/world.class.js
@@ -38,11 +38,12 @@ class World {
         this.ctx.translate(this.camera_x, 0);
 
         this.addObjectsToMap(this.level.backgroundObjects);
-        this.addToMap(this.statusBar);
+        
         this.addToMap(this.character);
         this.addObjectsToMap(this.level.clouds);
         this.addObjectsToMap(this.level.enemies);
         this.addObjectsToMap(this.level.coins);
+        this.addToMap(this.statusBar);
 
 
         this.ctx.translate(-this.camera_x, 0);
