3ee1542f40ecc290302e49d10344901bef5c4596
OtschoP <35844438+OtschoP@users.noreply.github.com>
2026-04-10 16:10:38 +0200

    changed speed of throw

--- models/throwable-object.class.js
+++ models/throwable-object.class.js
@@ -1,20 +1,22 @@
 class ThrowableObject extends MoveableObject {
     speedX = 0;
 
-    constructor(x, y) {
+    constructor(x, y, otherDirection = false) {
         super().loadImage('img/6_salsa_bottle/salsa_bottle.png');
         this.x = x;
         this.y = y;
+        this.otherDirection = otherDirection;
         this.height = 60;
         this.width = 50;
         this.throw();
     }
 
     throw(){
-        this.speedY = 30;
+        this.speedY = 20;
+        this.speedX = 10;
         this.applyGravity();
         setInterval(() => {
-            this.x += 7;
+            this.x += this.otherDirection ? -this.speedX : this.speedX;
         }, 25);
     }
 }
